ca2fc51c2b97d195a54a90efd3a49152ed7f7132
Mastin <martin.pavic97@gmail.com>
2021-12-08 21:28:00 +0100

    use sfpdfviewer

--- lib/screen/bus.dart
+++ lib/screen/bus.dart
@@ -1,67 +1,13 @@
 import 'package:brezovica/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class BusScreen extends StatefulWidget {
+class BusScreen extends StatelessWidget {
   const BusScreen({Key? key}) : super(key: key);
 
   @override
-  _BusScreenState createState() => _BusScreenState();
-}
-
-class _BusScreenState extends State<BusScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: Column(
-        children: [
-          GlassmorphicContainer(
-            width: 450,
-            height: 250,
-            borderRadius: 20,
-            blur: 20,
-            //alignment: Alignment.centerLeft,
-            border: 2,
-            linearGradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  const Color(0xFFffffff).withOpacity(0.3),
-                  const Color(0xFFFFFFFF).withOpacity(0.15),
-                ],
-                stops: const [
-                  0.1,
-                  1,
-                ]),
-            borderGradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                const Color(0xFFffffff).withOpacity(0.5),
-                const Color((0xFFFFFFFF)).withOpacity(0.5),
-              ],
-            ),
-            child: Column(children: const [
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Text(
-                  'Bus',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-              ElevatedButton(onPressed: onPressed, child: null)
-            ]),
-          ),
-        ],
-      ),
-    );
+    return SfPdfViewer.asset('assets/161.pdf');
   }
-}
-
-void onPressed() {
-  print("yo");
 }
