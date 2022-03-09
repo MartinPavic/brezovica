54c7a75bebf9989a946dd38c485123c35b72423a
Mastin <martin.pavic97@gmail.com>
2022-03-09 15:13:51 +0100

    Remove signup method

--- lib/service/supabase/supabase_auth.dart
+++ lib/service/supabase/supabase_auth.dart
@@ -1,13 +1,9 @@
+import 'package:brezovica/util/snackbar_mixin.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart' as riverpod;
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'supabase_auth.freezed.dart';
 
 class SupabaseAuthService {
   SupabaseAuthService(this._auth);
@@ -43,51 +39,16 @@ class SupabaseAuthService {
       (error, _) => error,
     );
   }
-
-  TaskEither<String, User> signUp(AuthData signUpData) {
-    return TaskEither.tryCatch(
-      () async {
-        final response = await signUpData.email.match(
-          (email) => _auth.signUp(email, signUpData.password),
-          () => signUpData.phoneNumber.match(
-            // Email empty, try phone number
-            (phoneNumber) =>
-                _auth.signUpWithPhone(phoneNumber, signUpData.password),
-            () => Future(() => GotrueSessionResponse(
-                error: GotrueError("Molimo unesite email ili broj telefona"))),
-          ),
-        );
-
-        if (response.error != null) {
-          throw response.error!;
-        }
-        if (response.user == null) {
-          throw Exception("WHAAT");
-        }
-        return response.user!;
-      },
-      (error, _) => error.toString(),
-    );
-  }
 }
 
 final supabaseAuthProvider = riverpod.Provider.autoDispose<SupabaseAuthService>(
     (_) => SupabaseAuthService(Supabase.instance.client.auth));
 
-@freezed
-class AuthData with _$AuthData {
-  const factory AuthData({
-    @Default(None()) Option<String> email,
-    @Default(None()) Option<String> phoneNumber,
-    required String password,
-  }) = _AuthData;
-}
-
 class AuthState<T extends StatefulWidget> extends SupabaseAuthState<T> {
   @override
   void onUnauthenticated() {
     if (mounted) {
-      // Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
+      Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
     }
   }
 
@@ -103,7 +64,7 @@ class AuthState<T extends StatefulWidget> extends SupabaseAuthState<T> {
 
   @override
   void onErrorAuthenticating(String message) {
-    //context.showErrorSnackBar(message: message);
+    context.showErrorSnackBar(message: message);
   }
 }
 
